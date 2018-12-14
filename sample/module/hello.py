c3e7db49cf04bd4fd648a9bb1dae353bb9fe0bf5
WizStriver1 <1954620922@qq.com>
2018-12-14 11:41:24 +0800

    自建模块hello.py

---
+++ sample/module/hello.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' a test module '
+
+__author__ = 'WizStriver1'
+
+import sys
+
+def test():
+	args = sys.argv
+	print('args', args)
+	if len(args) == 1:
+		print('Hello, %s!' % args[0])
+	elif len(args) == 2:
+		print('Meet again, %s!' % args[1])
+	else:
+		print('Too many arguments!')
+
+print(__name__)
+if __name__ == '__main__':
+	test()
