a2bb4feadeaeb32ddfffc262d6d7b6ecfe24e0a6
WizStriver1 <1954620922@qq.com>
2019-03-07 11:34:43 +0800

    Flask 添加Cookie, Redirect and Errors

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,6 @@
 #!/usr/env python3
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, url_for, redirect, abort
 
 from werkzeug.utils import secure_filename
 
@@ -11,6 +11,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     username = request.cookies.get('username')
+    if not username:
+         resp = make_response(render_template('child.html', items=['Kobe', 'Wade'], msg=None))
+         resp.set_cookie('username', 'server_set')
+         return resp
     return render_template('child.html', items=['Kobe', 'Wade'], msg=username)
 
 @app.route('/post/<post_id>')
@@ -45,5 +49,18 @@ def upload():
         f.save('uploads/' + secure_filename(f.filename))
         msg = 'Yes, we finished upload'
     return render_template('upload.html', msg = msg)
+
+@app.route('/toredirect')
+def toredirect():
+    return redirect(url_for('notfound'))
+
+@app.route('/notfound')
+def notfound():
+    abort(404)
+    return render_template('not-found.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('not-found.html', msg='render by errorhandler')
 
 app.run(host='0.0.0.0', port=6100, debug=config.debug)
